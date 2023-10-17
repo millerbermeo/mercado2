91980a6c24c4b9e52e55cb84ee703450940429cd
millerbermeo <millerbermeo30@gmail.com>
2023-10-17 11:34:29 -0500

    second commit

--- mercadoLibre.js
+++ mercadoLibre.js
@@ -2,13 +2,17 @@
 let inputBusqueda = document.getElementById("inputBusqueda");
 
 class Automovil {
-    constructor(marca, modelo) {
+    constructor(marca, modelo, precio,year, km, city) {
         this.marca = marca;
         this.modelo= modelo;
+        this.precio= precio;
+        this.year= year;
+        this.km= km;
+        this.city= city;
     }
 }
 
-let auto1 = new Automovil("BMW", "G63");
+let auto1 = new Automovil("BMW", "s1000rr", 104000000, "2023", "22.000", "Pitalito - Huila");
 
 
 
@@ -29,11 +33,53 @@ window.addEventListener('keydown', function(event) {
         
         boxImg.appendChild(imgAuto);
         imgAuto.setAttribute("src","motogp.webp");
-        imgAuto.setAttribute("class", "compu-mercado");
+        imgAuto.setAttribute("class", "img-auto");
     
         // Caja para la informacion
         let boxInfo = document.createElement("div");
         boxProducto.appendChild(boxInfo);
         boxInfo.setAttribute("class", "box-info");
+
+        let marca = document.createElement("label")
+        boxInfo.appendChild(marca)
+        
+        let txtNodeMarca = this.document.createTextNode(auto1.marca + " " + auto1.modelo)
+        marca.appendChild(txtNodeMarca)
+
+
+
+        let precio = document.createElement("label")
+        boxInfo.appendChild(precio)
+
+
+        let prescioSrt = Intl.NumberFormat("de-DE").format(auto1.precio)
+
+        let txtNodePrecio = document.createTextNode(`$ ${prescioSrt}`)
+        precio.appendChild(txtNodePrecio)
+        precio.setAttribute("class", "precio")
+
+
+
+        let kilometraje = document.createElement("label")
+        boxInfo.appendChild(kilometraje)
+
+        let txtNodeKm = document.createTextNode(`${auto1.year} . ${auto1.km} km ${auto1.city}`)
+        kilometraje.appendChild(txtNodeKm)
+        kilometraje.setAttribute("class", "kilometraje")
+
+
+
+
+        let boxCorazon = document.createElement("div")
+        boxImg.appendChild(boxCorazon);
+
+        let iconCorazon = document.createElement("i");
+
+        boxCorazon.appendChild(iconCorazon)
+        boxCorazon.setAttribute("class", "box-corazon")
+        iconCorazon.setAttribute("class", "icon-corazon fa-regular fa-heart")
+
+
+    
     }
 })
